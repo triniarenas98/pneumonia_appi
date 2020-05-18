5e53b97de47e21d420310e35bc1f4c99676314b6
TRINIDAD_ARENAS <trinidad.arenas@uvic.cat>
2020-05-18 20:02:08 +0200

    fi

--- ResNet_18/code/__torch__.py
+++
@@ -1,7 +0,0 @@
-class MyResnet_18(Module):
-  __parameters__ = []
-  training : bool
-  resnet : __torch__.torchvision.models.resnet.ResNet
-  def forward(self: __torch__.MyResnet_18,
-    x: Tensor) -> Tensor:
-    return (self.resnet).forward(x, )
